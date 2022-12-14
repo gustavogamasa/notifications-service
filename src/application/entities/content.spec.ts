bf8b3025c699b467eddf3de4e1ef75e6e5edffdf
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2022-12-14 00:45:37 -0300

    New jest tests

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,6 +1,16 @@
 import { Content } from './content';
+
 test('It should be able to create a notification content', () => {
     const content = new Content('Nova notificação');
 
     expect(content).toBeTruthy();
-})
+});
+
+test('It should not be possible to create a notification content with less than 5 characters', () => {
+    expect(()=> new Content('abc')).toThrow();
+});
+
+
+test('It should not be possible to create a notification content with more than 240 characters', () => {
+    expect(()=> new Content('abc'.repeat(241))).toThrow();
+});
